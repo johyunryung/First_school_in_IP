c39b96847122268d9dfca4404e0c848dbb80739f
jiyeon1117 <s2019w24@e-mirim.hs.kr>
2021-05-26 01:38:24 +0900

    웹 사이트 미니배너 & 헤더 클릭 시 색 변하는 js 개발

---
+++ js/header-color-change.js
@@ -0,0 +1,10 @@
+function toggleImg() {
+  var toggle = document.getElementById("img");
+  toggle.value = "로그인";
+}
+
+/* 해당 창 클릭시 텍스트 색깔 변경 */
+window.onload = function() {
+  var click = document.getElementById("click");
+  click.style.color = "#197b30";
+}
