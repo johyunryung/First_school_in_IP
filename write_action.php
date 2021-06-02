7aa400b4eda189c7f1d4fec87bb91b05d8b7861c
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2021-06-02 11:36:06 +0900

    글쓰기 폼 완성 및 php 병합, mysql 연결 테스트 실패

---
+++ write_action.php
@@ -0,0 +1,30 @@
+<?php
+        $connect = mysqli_connect("localhost", "bord", "root", "mirim2") or die("fail");
+                $number = $_POST[number];                      //Writer
+                $categori = $_POST[categori];                        //Password
+                $title = $_POST[title];                  //Title
+                $content = $_POST[content];              //Content
+                $date = $_POST[date]; 
+                $c_content = $_POST[c_content];//Date
+ 
+                $URL = './QuestionWrite.php';                   
+
+                $categori = $_POST[categori];                        //Password
+                $query = "insert into bord (number, categori, title, content, date, c_content) 
+                        values(null, '$category', '$title', '$content', null, null)";
+ 
+ 
+                $result = $connect->query($query);
+                if($result){
+?>                  <script>
+                        alert("<?php echo "글이 등록되었습니다."?>");
+                        location.replace("<?php echo $URL?>");
+                    </script>
+<?php
+                }
+                else{
+                        echo "FAIL";
+                }
+ 
+                mysqli_close($connect);
+?>
