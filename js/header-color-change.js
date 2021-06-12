2b7036bb786e17d658f9dfa87b3536e467e737d8
jiyeon1117 <s2019w24@e-mirim.hs.kr>
2021-06-12 20:26:03 +0900

    버튼 색깔 수정 js

--- js/header-color-change.js
+++ js/header-color-change.js
@@ -1,10 +1,26 @@
-function toggleImg() {
-  var toggle = document.getElementById("img");
-  toggle.value = "로그인";
+function toggleButton1() {
+  var toggle1 = document.getElementById("btn1");
+  toggle1.style.backgroundColor = "#007b4d";
+}
+
+function toggleButton2() {
+  var toggle2 = document.getElementById("btn2");
+  toggle2.style.backgroundColor = "#007b4d";
+}
+
+function toggleButton3() {
+  var toggle3 = document.getElementById("btn3");
+  toggle3.style.backgroundColor = "#007b4d";
+}
+
+function toggleButton4() {
+  var toggle4 = document.getElementById("btn4");
+  toggle4.style.backgroundColor = "#007b4d";
 }
 
 /* 해당 창 클릭시 텍스트 색깔 변경 */
 window.onload = function() {
   var click = document.getElementById("click");
+
   click.style.color = "#197b30";
 }
